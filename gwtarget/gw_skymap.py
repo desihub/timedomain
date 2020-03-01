83740ac9469fecea64c609616fa09f7f4c381e95
Segev BenZvi <sybenzvi@gmail.com>
2020-03-01 14:21:47 -0800

    Add plotting function.

--- gwtarget/gw_skymap.py
+++ gwtarget/gw_skymap.py
@@ -8,9 +8,14 @@ Updated SYB, Dec 2020.
 import os
 import numpy  as np
 import healpy as hp
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-import meander #pip install this to draw contours around skymap
+
+try:
+    import meander  # pip install this to draw contours around skymap
+except ImportError as e:
+    log.error(e)
+    raise SystemExit
 
 from glob import glob
 from urllib.request import urlretrieve
@@ -18,6 +23,7 @@ from matplotlib import cm
 from astropy.io import fits
 
 from argparse import ArgumentParser
+
 
 def compute_quantiles(proportions, samples):
     """Get quantiles of HEALPix probability map.
@@ -45,6 +51,7 @@ def compute_quantiles(proportions, samples):
         level = (sorted_samples[level_index] + (sorted_samples[level_index+1] if level_index+1 < len(samples) else 0)) / 2.0
         levels.append(level)
     return levels
+
 
 def compute_contours(proportions, samples):
     """Plot containment contour around desired level. E.g 90% containment of a
@@ -86,6 +93,95 @@ def compute_contours(proportions, samples):
             phi_list.append(phi)
 
     return theta_list, phi_list
+
+
+def plot_gwmap(lvc_healpix_file, levels=[0.5, 0.9]):
+    """Plot the GW map with the DESI footprint.
+    
+    Parameters
+    ----------
+    lvc_healpix_file : str
+        Relative or absolute path to LIGO/Virgo HEALPix angular reconstruction file.
+    levels : list
+        List of credible interval thresholds, e.g., 0.5, 0.9, etc.
+    
+    Returns
+    -------
+    fig : matplotlib.Figure
+        Figure object for accessing or saving a plot.
+    """
+    # Read metadata from FITS.
+    hdus = fits.open(lvc_healpix_file)
+    header = hdus[1].header
+
+    # instruments = header['INSTRUME']
+    distmean = header['DISTMEAN']
+    diststd = header['DISTSTD']
+    origin = header['ORIGIN']
+    date = header['DATE']
+    
+    # Read HEALPix map.
+    gwmap = hp.read_map(lvc_healpix_file)
+
+    # Compute GW contours.
+    prob64 = hp.pixelfunc.ud_grade(gwmap, 64) #reduce nside to make it faster
+    prob64 = prob64/np.sum(prob64)
+    pixels = np.arange(prob64.size)
+    #sample_points = np.array(hp.pix2ang(nside,pixels)).T
+    theta_contour, phi_contour = compute_contours(levels, prob64)
+
+    cmap = mpl.cm.OrRd
+    cmap.set_under('w')
+
+    # Access DESI contours.
+    nside = hp.pixelfunc.get_nside(gwmap)
+    desi_mask = hp.read_map('desi_mask_nside{:04d}.fits'.format(nside))
+    probs = np.copy(gwmap)
+    probs[desi_mask == 0] = hp.UNSEEN
+
+    hp.mollview(probs, cbar=True, unit=r'probability', title='{} {}'.format(origin, date),
+                min=0, max=2e-4, flip='astro', rot=180, cmap=cmap)
+    hp.graticule(ls=':', alpha=0.5, dpar=30, dmer=45)
+
+    thmin, thmax = 1e99, -1e99
+    phmin, phmax = 1e99, -1e99
+    for i, (tc, pc) in enumerate(zip(theta_contour, phi_contour)):
+        thmin = np.minimum(thmin, np.min(tc))
+        thmax = np.maximum(thmax, np.max(tc))
+        phmin = np.minimum(phmin, np.min(pc))
+        phmax = np.maximum(phmax, np.max(pc))
+        hp.projplot(tc, pc, linewidth=1, c='k')
+
+    ramin, ramax = np.degrees(phmin), np.degrees(phmax)
+    decmin, decmax = 90-np.degrees(thmax), 90-np.degrees(thmin)
+
+    ax = plt.gca()
+
+    # Label latitude lines.
+    ax.text( 2.00,  0.10, r'$0^\circ$', horizontalalignment='left')
+    ax.text( 1.80,  0.45, r'$30^\circ$', horizontalalignment='left')
+    ax.text( 1.30,  0.80, r'$60^\circ$', horizontalalignment='left')
+    ax.text( 1.83, -0.45, r'$-30^\circ$', horizontalalignment='left')
+    ax.text( 1.33, -0.80, r'$-60^\circ$', horizontalalignment='left')
+    ax.text(-2.00,  0.10, r'$0^\circ$', horizontalalignment='right')
+    ax.text(-1.80,  0.45, r'$30^\circ$', horizontalalignment='right')
+    ax.text(-1.30,  0.80, r'$60^\circ$', horizontalalignment='right')
+    ax.text(-1.85, -0.45, r'$-30^\circ$', horizontalalignment='right')
+    ax.text(-1.35, -0.80, r'$-60^\circ$', horizontalalignment='right')
+
+    # Label longitude lines.
+    ax.text( 2.0, -0.15, r'0$^\mathrm{h}$', horizontalalignment='center')
+    ax.text( 1.5, -0.15, r'3$^\mathrm{h}$', horizontalalignment='center')
+    ax.text( 1.0, -0.15, r'6$^\mathrm{h}$', horizontalalignment='center')
+    ax.text( 0.5, -0.15, r'9$^\mathrm{h}$', horizontalalignment='center')
+    ax.text( 0.0, -0.15, r'12$^\mathrm{h}$', horizontalalignment='center')
+    ax.text(-0.5, -0.15, r'15$^\mathrm{h}$', horizontalalignment='center')
+    ax.text(-1.0, -0.15, r'18$^\mathrm{h}$', horizontalalignment='center')
+    ax.text(-1.5, -0.15, r'21$^\mathrm{h}$', horizontalalignment='center')
+    ax.text(-2.0, -0.15, r'24$^\mathrm{h}$', horizontalalignment='center')
+    
+    fig = plt.gcf()
+    return fig
 
 
 if __name__ == '__main__':
