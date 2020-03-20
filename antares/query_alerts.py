db27180655f16cfb72464b7f40ebcd4158b2e260
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-03-20 11:23:14 -0400

    Modulize the query function.

--- antares/query_alerts.py
+++ antares/query_alerts.py
@@ -1,64 +1,92 @@
-import argparse
-from math import *
+import numpy as np
+from datetime import datetime
+from astropy.io import ascii
+from astropy.time import Time
+from argparse import ArgumentParser
 
-parser = argparse.ArgumentParser(description='Client API to query ANTARES alert database')
-parser.add_argument("--ra", default=None, type=float, help="RA (J2000)")
-parser.add_argument("--dec", default=None, type=float, help="Dec (J2000)")
-parser.add_argument("--tobs", default=None, type=float, help="Time of observation (MJD)")
+from antares_client.search import search, download
 
-args = parser.parse_args()
-ra = args.ra
-dec = args.dec
-tobs = args.tobs
-if ra==None or dec==None or tobs==None:
-    print ('Usage: query_alert.py --ra --dec --tobs')
-    exit()
-    
+def build_query(ra0, dec0, fov, date):
+    """Generate a query (a Python dictionary) to submit to the ANTARES client.
 
-dra = 1.5*cos(dec/180.*pi)
-ddec = 1.5
-#dt=3
-#print (tobs-dt)
+    Parameters
+    ----------
+    ra0 : float or None
+        Central RA for object search, in deg.
+    dec0 : float or None
+        Central declination for object search, in deg.
+    fov : float
+        Side length of box for search, in deg.
+    date : str
+        Start date for search; format is YYYY-MM-DD.
 
-query = {
-    "query": {
-        "bool": {
-            "must": [
-                {
-                    "match":{
-                        "properties.snfilter_last_proc_status": "*DESI*"
-                    }
-                },
-                {
-                    "range": {
-                        "ra": {
-                            "gte": ra-dra,
-                            "lte": ra+dra,
-                        }
-                    }
-                },
-                {
-                    "range": {
-                        "dec": {
-                            "gte": dec-ddec,
-                            "lte": dec+ddec,
-                        }
-                    }
-                },
-                {
-                    "range": {
-                        "mjd": {
-                            "gte": tobs,
-                        }
-                    }
-                }
-             ]
-        }
-    }
-}
+    Returns
+    -------
+    query : dict
+        An ElasticSearch dictionary.
+    """
 
-from antares_client.search import search
-result_set = search(query)
+    # Build up the query.
+    query = { 'query': { 'bool': { 'must': [] } } }
 
-from antares_client.search import download
-result_set = download(query, "results.csv", output_format="csv", decompress=True)
+    # desi_candidate_test data stream:
+    # snfilter_last_proc_status should have a string like "Locus has two or
+    # more detections and is in DESI brightness range. Triggering."
+    query['query']['bool']['must'].append(
+        { 'match':{ 'properties.snfilter_last_proc_status': '*DESI*' } })
+
+    # Set up the declination search.
+    if dec0 is not None:
+        ddec = 0.5 * fov
+        # dra / cos(dec) ensures an equal-area search rectangle.
+        dra = 0.5*fov / np.cos(np.radians(dec0))
+
+        query['query']['bool']['must'].append(
+            {'range': {'dec':{ 'gte':dec0-ddec, 'lte':dec0+ddec, } } })
+    else:
+        dra = 0.5*fov
+
+    # Set up the RA search.
+    if ra0 is not None:
+        query['query']['bool']['must'].append(
+            {'range': {'ra':{ 'gte':ra0-dra, 'lte':ra0+dra, } } })
+
+    # Set up the cumulative date search.
+    if date is not None:
+        tobs = Time(date).mjd
+        query['query']['bool']['must'].append(
+            {'range': {'mjd':{ 'gte':tobs, } } })
+
+    return query
+
+if __name__ == '__main__':
+
+    today = datetime.today()
+
+    parser = ArgumentParser(description='Client API to query ANTARES alert DB')
+    parser.add_argument('--ra', default=None, type=float,
+                        help='RA (J2000), in deg')
+    parser.add_argument('--dec', default=None, type=float,
+                        help='Dec (J2000), in deg')
+    parser.add_argument('--tobs', default=datetime.today().strftime('%Y-%m-%d'),
+                        help='Obs date [YYYY-MM-DD]')
+    args = parser.parse_args()
+
+    # Create query dict for ANTARES stream search.
+    query = build_query(ra0=args.ra, dec0=args.dec, fov=3.2, date=args.tobs)
+    print(query)
+
+    #result_set = search(query)
+    #print(result_set)
+
+    outfile = 'results_antares'
+    if args.ra is not None:
+        outfile = '{}_ra{:03.1f}'.format(outfile, args.ra)
+    if args.dec is not None:
+        outfile = '{}_dec{:03.1f}'.format(outfile, args.dec)
+    if args.tobs is not None:
+        outfile = '{}_{}'.format(outfile, args.tobs)
+    outfile += '.csv'
+
+    result_set = download(query, outfile, output_format='csv', decompress=True)
+
