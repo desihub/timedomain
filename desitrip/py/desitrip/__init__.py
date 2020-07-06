2d7a01cc245d6d8ed66eeff39c2ccd008d7da832
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-07-06 12:04:40 -0400

    Add setup script and module initialization.

---
+++ desitrip/py/desitrip/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+========
+desitrip
+========
+
+DESI transient identification pipeline.
+"""
+from __future__ import absolute_import
+from ._version import __version__
---
+++ desitrip/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import absolute_import, division, print_function
+#
+# Standard imports
+#
+import glob
+import os
+import sys
+#
+# setuptools' sdist command ignores MANIFEST.in
+#
+from distutils.command.sdist import sdist as DistutilsSdist
+from setuptools import setup, find_packages
+#
+# DESI support code.
+#
+from desiutil.setup import DesiTest, DesiVersion, get_version
+#
+# Begin setup
+#
+setup_keywords = dict()
+#
+# THESE SETTINGS NEED TO BE CHANGED FOR EVERY PRODUCT.
+#
+setup_keywords['name'] = 'desitrip'
+setup_keywords['description'] = 'DESI transient identification pipeline'
+setup_keywords['author'] = 'DESI Collaboration'
+setup_keywords['author_email'] = 'desi-timedomain@desi.lbl.gov'
+setup_keywords['license'] = 'BSD'
+setup_keywords['url'] = 'https://github.com/desihub/timedomain/desitrip'
+#
+# END OF SETTINGS THAT NEED TO BE CHANGED.
+#
+setup_keywords['version'] = get_version(setup_keywords['name'])
+#
+# Use README.rst as long_description.
+#
+setup_keywords['long_description'] = ''
+if os.path.exists('README.rst'):
+    with open('README.rst') as readme:
+        setup_keywords['long_description'] = readme.read()
+#
+# Set other keywords for the setup function.  These are automated, & should
+# be left alone unless you are an expert.
+#
+# Treat everything in bin/ except *.rst as a script to be installed.
+#
+if os.path.isdir('bin'):
+    setup_keywords['scripts'] = [fname for fname in glob.glob(os.path.join('bin', '*'))
+        if not os.path.basename(fname).endswith('.rst')]
+setup_keywords['provides'] = [setup_keywords['name']]
+setup_keywords['requires'] = ['Python (>2.7.0)']
+# setup_keywords['install_requires'] = ['Python (>2.7.0)']
+setup_keywords['zip_safe'] = False
+setup_keywords['use_2to3'] = False
+setup_keywords['packages'] = find_packages('py')
+setup_keywords['package_dir'] = {'':'py'}
+setup_keywords['cmdclass'] = {'version': DesiVersion, 'test': DesiTest, 'sdist': DistutilsSdist}
+setup_keywords['test_suite']='{name}.test.test_suite'.format(**setup_keywords)
+#
+# Autogenerate command-line scripts.
+#
+# setup_keywords['entry_points'] = {'console_scripts':['desiInstall = desiutil.install.main:main']}
+#
+# Add internal data directories.
+#
+setup_keywords['package_data'] = {'desitrip': ['data/*', 'test/data/*']}
+#
+# Run setup command.
+#
+setup(**setup_keywords)
