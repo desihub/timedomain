d0365268adbcdb79492e2300c1df21f1d8ba176d
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-10-19 13:28:11 -0400

    Touch up plotting from script.

--- gwtarget/gw_skymap.py
+++ gwtarget/gw_skymap.py
@@ -23,7 +23,7 @@ from urllib.request import urlretrieve
 from matplotlib import cm
 from astropy.io import fits
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
 def compute_quantiles(proportions, samples):
@@ -141,7 +141,9 @@ def plot_gwmap(lvc_healpix_file, levels=[0.5, 0.9], rot=255):
     probs = np.copy(gwmap)
     probs[desi_mask == 0] = hp.UNSEEN
 
-    hp.mollview(probs, cbar=True, unit=r'probability', title='{} {}'.format(origin, date),
+    fig = plt.figure(num=1, figsize=(10,6))
+    hp.mollview(probs, fig=1, cbar=True, unit=r'$dp/d\Omega$ [deg$^{-2}$]',
+                title='{} {}'.format(origin, date),
                 min=0, max=2e-4, flip='astro', rot=rot, cmap=cmap)
     hp.graticule(ls=':', alpha=0.5, dpar=30, dmer=45)
 
@@ -156,7 +158,7 @@ def plot_gwmap(lvc_healpix_file, levels=[0.5, 0.9], rot=255):
         
     logging.info('RA_min={:.1f}, RA_max={:.1f}, Dec_min={:.1f}, Dec_max={:.1f}'.format(ramin, ramax, decmin, decmax))
 
-    ax = plt.gca()
+#    ax = plt.gca()
 
     # Label latitude lines.
     for _dec in [-60,-30,0,30,60]:
@@ -169,112 +171,57 @@ def plot_gwmap(lvc_healpix_file, levels=[0.5, 0.9], rot=255):
         hp.projtext(_ra, -40, r'{:d}$^\circ$'.format(_ra),
                     horizontalalignment='center', fontsize=12, lonlat=True)
     
-    fig = plt.gcf()
+#    fig = plt.gcf()
     return fig
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='GW Event Plotter')
-    parser.add_argument('event_id', nargs=1,
+    p = ArgumentParser(description='GW Event Plotter',
+                       formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('-d', '--display', dest='display',
+                   action='store_true', default=False,
+                   help='Display plot')
+
+    sp = p.add_subparsers(title='subcommands', description='valid subcommands',
+                          help='Additional help.')
+
+    # Event mode: access GW event from GraceDB using ID number.
+    pemode = sp.add_parser('eventmode', help='Access event from GraceDB.')
+    pemode.add_argument('event_id', nargs=1,
                         type=str,
                         help='LVC event ID')
-    parser.add_argument('-d', '--display', dest='display',
-                        action='store_true', default=False,
-                        help='Display plot')
-    parser.add_argument('-t', '--type', dest='event_type',
+    pemode.add_argument('-t', '--type', dest='event_type',
                         default=None, type=str,
                         help='Event type [BBH, BNS, ..]')
-    args = parser.parse_args()
+
+    # File mode: access GW event in a downloaded file.
+    pfmode = sp.add_parser('filemode', help='Access event from FITS file.')
+    pfmode.add_argument('fitsfile', nargs=1,
+                        type=str,
+                        help='FITS file with GW maps.')
+
+    args = p.parse_args()
     
     # Fits file containing Gravitational wave skymap
-    event_id = args.event_id[0]
-    fitsFile = 'bayestar_{}.fits.gz'.format(event_id)
-    if not os.path.isfile(fitsFile):
-        url = 'https://gracedb.ligo.org/api/superevents/{}/files/bayestar.fits.gz'.format(event_id)
-        print('Downloading {}'.format(url))
-        urlretrieve(url, fitsFile)
+    if 'event_id' in args:
+        event_id = args.event_id[0]
+        fitsFile = 'bayestar_{}.fits.gz'.format(event_id)
+        if not os.path.isfile(fitsFile):
+            url = 'https://gracedb.ligo.org/api/superevents/{}/files/bayestar.fits.gz'.format(event_id)
+            print('Downloading {}'.format(url))
+            urlretrieve(url, fitsFile)
+    elif 'fitsfile' in args:
+        fitsFile = args.fitsfile[0]
     
-    # Read metadata.
-    hdus = fits.open(fitsFile)
-    header = hdus[1].header
-    instruments = header['INSTRUME']
-    distmean = header['DISTMEAN']
-    diststd = header['DISTSTD']
+    fig = plot_gwmap(fitsFile)
+
+    # Output plot to a PDF file.
+    if 'event_id' in args:
+        fig.savefig('gw_desi_{}.pdf'.format(event_id))
+    elif 'fitsfile' in args:
+        basename = fitsFile.split('.')[0]
+        fig.savefig('gw_desi_{}.pdf'.format(basename))
     
-    # Read map and get probabilities
-    probs = hp.read_map(fitsFile)
-    nside = hp.pixelfunc.get_nside(probs)
-    
-    # Choose color map and set background to white
-    cmap = cm.OrRd
-    cmap.set_under("w")
-    
-    # Compute GW contours.
-    prob64 = hp.pixelfunc.ud_grade(probs, 64) #reduce nside to make it faster
-    prob64 = prob64/np.sum(prob64)
-    pixels = np.arange(prob64.size)
-    #sample_points = np.array(hp.pix2ang(nside,pixels)).T
-    levels = [0.50, 0.90]
-    theta_contour, phi_contour = compute_contours(levels, prob64)
-    
-    # Access DESI contours.
-    desi_mask = hp.read_map('desi_mask_nside{:04d}.fits'.format(nside))
-    probs[desi_mask == 0] = hp.UNSEEN
-    
-    # Plot GW skymap in Mollweide projection
-    hp.mollview(probs, cbar=True, unit=r'probability', min=0, max=3e-5, rot=180, cmap=cmap)
-    hp.graticule(ls=':', alpha=0.5, dpar=30, dmer=45) # Set grid lines
-    
-    # Draw containment contour around GW skymap
-    nregion = len(theta_contour) // len(levels)
-    # ls = ['-', '--', '-.']
-    # label = ''
-    for i, (tc, pc) in enumerate(zip(theta_contour, phi_contour)):
-        hp.projplot(tc, pc, linewidth=1, c='k')
-    #     j = i // nregion
-    #     print(len(theta_contour), nregion, i, j)
-    #     newlabel = '{:g}% credible region'.format(100*levels[j])
-    #     if newlabel == label:
-    #         hp.projplot(tc, pc, linewidth=1, c='k', linestyle=ls[j])
-    #     else:
-    #         hp.projplot(tc, pc, linewidth=1, c='k', linestyle=ls[j], label=newlabel)
-    #         label = newlabel
-    
-    ax = plt.gca()
-    
-    # Label latitude lines.
-    ax.text( 2.00,  0.10, r'$0^\circ$', horizontalalignment='left')
-    ax.text( 1.80,  0.45, r'$30^\circ$', horizontalalignment='left')
-    ax.text( 1.30,  0.80, r'$60^\circ$', horizontalalignment='left')
-    ax.text( 1.83, -0.45, r'$-30^\circ$', horizontalalignment='left')
-    ax.text( 1.33, -0.80, r'$-60^\circ$', horizontalalignment='left')
-    ax.text(-2.00,  0.10, r'$0^\circ$', horizontalalignment='right')
-    ax.text(-1.80,  0.45, r'$30^\circ$', horizontalalignment='right')
-    ax.text(-1.30,  0.80, r'$60^\circ$', horizontalalignment='right')
-    ax.text(-1.85, -0.45, r'$-30^\circ$', horizontalalignment='right')
-    ax.text(-1.35, -0.80, r'$-60^\circ$', horizontalalignment='right')
-    
-    # Label longitude lines.
-    ax.text( 2.0, -0.15, r'0$^\mathrm{h}$', horizontalalignment='center')
-    ax.text( 1.5, -0.15, r'3$^\mathrm{h}$', horizontalalignment='center')
-    ax.text( 1.0, -0.15, r'6$^\mathrm{h}$', horizontalalignment='center')
-    ax.text( 0.5, -0.15, r'9$^\mathrm{h}$', horizontalalignment='center')
-    ax.text( 0.0, -0.15, r'12$^\mathrm{h}$', horizontalalignment='center')
-    ax.text(-0.5, -0.15, r'15$^\mathrm{h}$', horizontalalignment='center')
-    ax.text(-1.0, -0.15, r'18$^\mathrm{h}$', horizontalalignment='center')
-    ax.text(-1.5, -0.15, r'21$^\mathrm{h}$', horizontalalignment='center')
-    ax.text(-2.0, -0.15, r'24$^\mathrm{h}$', horizontalalignment='center')
-    
-    # Label participating detectors.
-    ax.text(-1.75, -1.1, instruments, horizontalalignment='left', fontsize=12)
-    
-    if args.event_type is not None:
-        plt.title('{} : {}, $D_L={:.1f}\\pm{:.1f}$ Mpc'.format(event_id, args.event_type, distmean, diststd), fontsize=12)
-    else:
-        plt.title('{} : $D_L={:.1f}\\pm{:.1f}$ Mpc'.format(event_id, distmean, diststd), fontsize=12)
-    #plt.legend(loc='lower right', bbox_to_anchor=(1.02, -0.075), fontsize=9,
-    #           frameon=False, facecolor=None)
-    plt.savefig('gw_desi_{}.pdf'.format(event_id))
-    
+    # Display results.
     if args.display:
         plt.show()
