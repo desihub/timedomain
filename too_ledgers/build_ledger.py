a9a331e0fce95a8d3a15ffe2fc20446404ae0f2d
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2022-04-27 19:02:00 -0400

    Clean up argument parsing.

--- too_ledgers/build_ledger.py
+++ too_ledgers/build_ledger.py
@@ -1,13 +1,18 @@
-from tooparse import TooLedgerMaker
-from argparse import ArgumentParser
+#!/usr/bin/env python
 
+from tooledger import TooLedgerMaker
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 if __name__ == '__main__':
-    p = ArgumentParser(description='ToO Ledger maker')
-    p.add_argument('inputfile', nargs=1, help='Input TOO file.')
+    p = ArgumentParser(description='ToO Ledger maker',
+                       formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument('inputfile', nargs=1,
+                   help='Input TOO file.')
+    p.add_argument('-o', '--output', dest='output', default='ToO-input.ecsv',
+                   help='ToO ledger output filename.')
     args = p.parse_args()
 
     # Generate the ToO ledger.
     ledger = TooLedgerMaker()
-    ledger.build_too_ledger(args.inputfile[0], 'test.ecsv')
+    ledger.build_too_ledger(args.inputfile[0], args.output)
 
